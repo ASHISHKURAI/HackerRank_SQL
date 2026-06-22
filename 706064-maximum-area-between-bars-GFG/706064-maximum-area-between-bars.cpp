2979f92109f00509efc97c1a28765841dc08a59d
Ashish Kumar Rai <91818860+ASHISHKURAI@users.noreply.github.com>
2026-06-22 21:41:21 +0530

    Added solution - LeetHub

---
+++ 706064-maximum-area-between-bars-GFG/706064-maximum-area-between-bars.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int maxArea(vector<int> &height) {
+        // code here
+        int n=height.size();
+        int i=0, j=n-1;
+        int maxi=0;
+
+        while(i<j){
+            maxi=max(maxi, min(height[i], height[j])*(j-i-1));
+            if(height[i]<=height[j]) i++;
+            else j--;
+        }
+
+        return maxi;        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
