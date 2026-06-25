a05ebfc6a27f9b51ac270581a8cfaf953a375f2b
Ashish Kumar Rai <91818860+ASHISHKURAI@users.noreply.github.com>
2026-06-25 21:56:17 +0530

    Added solution - LeetHub

---
+++ 704744-ndigit-numbers-with-increasing-digits-GFG/704744-ndigit-numbers-with-increasing-digits.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void solve(int i,vector<int> &ans,int n,int cnt,int &sum){
+        cnt++;
+        sum = sum*10+i;
+        if(cnt == n){
+            ans.push_back(sum);
+            return ;
+        }
+        for(int j = i+1;j<=9;j++){
+            solve(j,ans,n,cnt,sum);
+            sum = sum/10;
+        }
+    }
+    vector<int> increasingNumbers(int n) {
+        vector<int> ans;
+        if(n == 1){
+            ans.push_back(0);
+        }
+        for(int i = 1;i<=9;i++){
+            int cnt = 0;
+            int sum = 0;
+            solve(i,ans,n,cnt,sum);
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
