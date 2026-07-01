94ecf5650bf1bf9f0e15d82fe4ab09152fb7f9fe
Ashish Kumar Rai <91818860+ASHISHKURAI@users.noreply.github.com>
2026-07-01 22:15:31 +0530

    Added solution - LeetHub

---
+++ 878902-print-square-wall-GFG/878902-print-square-wall.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string row;
+    for (int i = 0; i < n; i++)
+        row += "* ";
+
+    for (int i = 0; i < n; i++)
+        cout << row << endl;
+
+    return 0;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
